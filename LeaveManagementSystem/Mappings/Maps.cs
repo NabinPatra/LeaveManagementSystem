8b46850a79422f2b6d62ecaed0d53dfb512a996a
NabinPatra <nabinpatra1995@gmail.com>
2020-09-14 21:53:46 +0530

    Add project files.

---
+++ LeaveManagementSystem/Mappings/Maps.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using LeaveManagementSystem.Data;
+using LeaveManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+          
+        }
+        
+
+    }
+}
